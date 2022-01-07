741b19e95f8216d88802f4b07f32c7933160ddc4
Arnulf Heimsbakk <Arnulf Heimsbakk>
2022-01-07 19:02:55 +0100

    powerline-go

--- powerline-go.sh
+++ powerline-go.sh
@@ -1,8 +1,8 @@
 function _update_ps1() {
-    PS1="$($GOPATH/bin/powerline-go -truncate-segment-width 12 -static-prompt-indicator -hostname-only-if-ssh -mode flat -numeric-exit-codes -error $? -jobs $(jobs -p | wc -l))"
+    PS1="$($(which powerline-go) -truncate-segment-width 12 -static-prompt-indicator -hostname-only-if-ssh -mode flat -numeric-exit-codes -error $? -jobs $(jobs -p | wc -l))"
 }
 
-if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
+if [ "$TERM" != "linux" ] && [ -f "$(which powerline-go)" ]; then
   PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
 fi
 
