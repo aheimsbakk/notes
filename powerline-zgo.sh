0423fa7196a629bc0027e2fd4b56d962022123d3
Arnulf Heimsbakk <arnulf.heimsbakk@gmail.com>
2023-04-19 19:23:22 +0200

    hostname shows it's a ssh connection

--- powerline-zgo.sh
+++ powerline-zgo.sh
@@ -1,5 +1,5 @@
 function _update_ps1() {
-    PS1="$(powerline-go -theme default -git-mode compact -cwd-mode plain -hostname-only-if-ssh -modules venv,user,host,ssh,cwd,perms,git,hg,jobs,exit,root -max-width 50 -colorize-hostname -static-prompt-indicator -mode flat -numeric-exit-codes -error $? -jobs $(jobs -p | wc -l))"
+    PS1="$(powerline-go -theme default -git-mode compact -cwd-mode plain -hostname-only-if-ssh -modules venv,user,host,cwd,perms,git,hg,jobs,exit,root -max-width 50 -colorize-hostname -static-prompt-indicator -mode flat -numeric-exit-codes -error $? -jobs $(jobs -p | wc -l))"
 }
 
 if [ "$TERM" != "linux" ] && [ -f "$(which powerline-go)" ]; then
