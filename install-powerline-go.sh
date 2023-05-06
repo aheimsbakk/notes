71eade72e67b1ea78d50df9269abce76da39ad32
Arnulf Heimsbakk <arnulf.heimsbakk@gmail.com>
2023-05-06 19:23:30 +0200

    faster powerline-go for fedora

--- install-powerline-go.sh
+++ install-powerline-go.sh
@@ -10,26 +10,27 @@ BASE_URL=https://raw.githubusercontent.com/aheimsbakk/notes/master
 
 source /etc/os-release
 
+rm -f /usr/local/bin/powerline-go
+if [ "$(uname -m)" = "armv7l" ]; then
+  wget -q -nc -O /usr/local/bin/powerline-go https://github.com/justjanne/powerline-go/releases/download/v1.22.1/powerline-go-linux-arm
+elif [ "$(uname -m)" = "aarch64" ]; then
+  wget -q -nc -O /usr/local/bin/powerline-go https://github.com/justjanne/powerline-go/releases/download/v1.22.1/powerline-go-linux-arm64
+else
+  wget -q -nc -O /usr/local/bin/powerline-go https://github.com/justjanne/powerline-go/releases/download/v1.22.1/powerline-go-linux-amd64
+fi
+chmod +x /usr/local/bin/powerline-go
+
 case "$ID" in
   debian | raspbian | ubuntu)
-    rm -f /usr/local/bin/powerline-go
     export DEBIAN_FRONTEND=noninteractive
-    if [ "$(uname -m)" = "armv7l" ]; then
-      wget -q -nc -O /usr/local/bin/powerline-go https://github.com/justjanne/powerline-go/releases/download/v1.22.1/powerline-go-linux-arm
-    elif [ "$(uname -m)" = "aarch64" ]; then
-      wget -q -nc -O /usr/local/bin/powerline-go https://github.com/justjanne/powerline-go/releases/download/v1.22.1/powerline-go-linux-arm64
-    else
-      wget -q -nc -O /usr/local/bin/powerline-go https://github.com/justjanne/powerline-go/releases/download/v1.22.1/powerline-go-linux-amd64
-    fi
-    chmod +x /usr/local/bin/powerline-go
     apt-get update
     apt-get -y install python3-pip git tmux vim-nox wget fonts-powerline
   ;;
   fedora)
-    dnf -y install python3-pip git tmux vim wget powerline-go
+    dnf -y install python3-pip git tmux vim wget
   ;;
   rhel | centos)
-    yum -y install python3-pip git tmux vim wget powerline-go
+    yum -y install python3-pip git tmux vim wget
   ;;
   *)
     echo Unknown OS.
