d7319a499db6539041d1f06a3a579bcefc071a69
Arnulf Heimsbakk <arnulf.heimsbakk@gmail.com>
2024-06-14 14:09:10 +0200

    oppdatert

--- powerline-zgo.sh
+++ powerline-zgo.sh
@@ -1,5 +1,5 @@
 function _update_ps1() {
-    PS1="$(powerline-go -git-mode compact -modules venv,user,host,cwd,perms,git,hg,jobs,exit,root -max-width 50 -colorize-hostname -static-prompt-indicator -mode flat -numeric-exit-codes -error $? -jobs $(jobs -p | wc -l))"
+    PS1="$(powerline-go -git-mode compact -modules venv,direnv,user,ssh,cwd,perms,git,hg,jobs,exit,newline,root -max-width 80 -colorize-hostname -static-prompt-indicator -mode flat -numeric-exit-codes -error $? -jobs $(jobs -p | wc -l))"
 }
 
 if [ "$TERM" != "linux" ] && [ -f "$(which powerline-go)" ]; then
