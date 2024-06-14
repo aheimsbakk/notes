38b450373cca9d15061cfaee542ced8e3e817d00
Arnulf Heimsbakk <arnulf.heimsbakk@gmail.com>
2024-06-14 20:01:47 +0200

    host if ssh

--- powerline-zgo.sh
+++ powerline-zgo.sh
@@ -1,5 +1,5 @@
 function _update_ps1() {
-  PS1="$(powerline-go -git-mode compact -modules venv,direnv,user,ssh,cwd,perms,terraform-workspace,kube,git,hg,jobs,exit,newline,root -max-width 90 -colorize-hostname -static-prompt-indicator -mode flat -numeric-exit-codes -error $? -jobs $(jobs -p | wc -l))"
+  PS1="$(powerline-go -hostname-only-if-ssh -git-mode compact -modules venv,direnv,user,host,cwd,perms,terraform-workspace,kube,git,hg,jobs,exit,newline,root -max-width 90 -colorize-hostname -static-prompt-indicator -mode flat -numeric-exit-codes -error $? -jobs $(jobs -p | wc -l))"
 }
 
 if [ "$TERM" != "linux" ] && [ -f "$(which powerline-go)" ]; then
